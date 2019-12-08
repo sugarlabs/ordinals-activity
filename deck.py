78b3f9701741f2c0dc83106db24643f19eec0921
root <marcus.jn.chong@gmail.com>
2019-12-08 12:01:30 -0500

    fixed copyrights

--- deck.py
+++ deck.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2019, Sugar Labs
+# Copyright (C) 2019 Walter Bender
+# Copyright (C) 2019 Marcus Chong
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2019, Sugar Labs
+# Copyright (C) 2019 Walter Bender
+# Copyright (C) 2019 Marcus Chong
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
